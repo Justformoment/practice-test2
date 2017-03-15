01057523ad3a40cdf36ac9d00563db22af281ae3
Justformoment <jast.net@mail.ru>
2017-03-16 00:11:50 +0100

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.practivetest2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.practivetest2.R;
+
+import static android.R.attr.name;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // PASTE CODE YOU WANT TO TEST HERE
+
+    }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+}
